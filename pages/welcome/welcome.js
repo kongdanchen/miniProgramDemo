9b91afab02ef3b289d4a1a02f9be1ad27aaab034
kongdanchen <kongdanchen@pku.edu.cn>
2018-04-13 13:14:17 +0800

    add list and template

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,23 +1,39 @@
 // pages/welcome.js
 
+
+var initData = {
+  hiddenFlag: false,
+  profile: {
+    nickName: "孔丹晨 - init",
+    avatarUrl: "/image/profilePhoto.jpg"
+  },
+  items: [{
+      name: "item1"
+    },
+    {
+      name: "item2"
+    },
+    {
+      name: "item3"
+    },
+    {
+      name: "item4"
+    },
+  ]
+};
 //[object Function]
 // 这里应该是对page函数的直接调用，里面是传进去的参数
-// Page() 函数用来注册一个页面。接受一个 object 参数
+// Page() 函数用来注册一个页面。接受一个 object 参数 
 Page({
 
-
-  /* 传入从 bindgetuserinfo中获取到的回调信息
-  是一个对象 */
-  getProfile(res) {
+  /* 直接搭建data对象并写入数据 */
+  data: initData,
+  /* 传入从 bindgetuserinfo中获取到的回调信息*/
+  getProfile: function (res) {
     // 将想要展示的数据以对象的形式传给页面
     this.setData({
       "profile": res.detail.userInfo
     });
-  },
-
-  /* 直接搭建data对象并写入数据 */
-  data:{
-
   }
 
 });
